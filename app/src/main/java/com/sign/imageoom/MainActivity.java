b1fde5eb48ff12c029c5274cbc4daf7157b83094
bangxu <1099698033@qq.com>
2019-01-18 18:05:52 +0800

    项目

--- app/src/main/java/com/sign/imageoom/MainActivity.java
+++ app/src/main/java/com/sign/imageoom/MainActivity.java
@@ -1,16 +1,37 @@
 package com.sign.imageoom;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Bitmap mCurrentBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView firImg = findViewById(R.id.first_img);
+        ImageView secImg = findViewById(R.id.second_img);
 
-        //shangchuancuole
-        //
+
+    }
+
+    /**
+     * 直接加载图片
+     * @param view
+     */
+    private void loadOriginalSize(ImageView view){
+
+    }
+
+    /**
+     * 压缩图片
+     * @param view
+     */
+    private void testPicOptimize(ImageView view){
+
     }
 }
